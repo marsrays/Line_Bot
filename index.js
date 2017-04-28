46f9256fc6c91535ffde2d54455b6d3230d18b3e
Ray <marsray@yxunistar.com>
2017-04-28 15:50:18 +0800

    update lib

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var linebot = require('linebot');
 var express = require('express');
+const app = express();
 
-var bot = linebot({
-    channelId: process.env.ChannelId,
+var LINEBot = require('line-messaging');
+var server = require('http').Server(app);
+
+
+var bot = LINEBot.create({
+    channelID: process.env.ChannelId,
     channelSecret: process.env.ChannelAccessToken,
-    channelAccessToken: process.env.ChannelSecret
-});
+    channelToken: process.env.ChannelSecret
+}, server);
 
 console.log("bot id:", process.env.ChannelId);
 console.log("bot Secret:", process.env.ChannelAccessToken);
 console.log("bot Token:", process.env.ChannelSecret);
 
-bot.on('message', function(event) {
-    console.log(event);
+app.use(bot.webhook('/webhook'));
+bot.on(LINEBot.Events.MESSAGE, function(replyToken, message) {
+    console.log(replyToken, message);
 });
-
-const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
-var server = app.listen(process.env.PORT || 8080, function() {
+server.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
     console.log("App now runing on port", port);
 });
