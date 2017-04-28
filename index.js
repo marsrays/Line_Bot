767d5a8758ba7885c76f5dd845da552eb024be37
marsrays <marsrays@gmail.com>
2017-04-28 14:52:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require('express');
 
 var bot = linebot({
     channelId: 1512220056,
-    channelSecret: "f717a97237db9bc70d4615c338a4a2cb",
-    channelAccessToken: "GYiKHZkEuDE0Mi6Lx1OLtQxaqzqgrs8HLYH4NK2eDQCCOGpllWeAETaChhiBJAk24CpQXXkex8MINujrs09uXd9QSiYOCw1peR/WUDBi+dB+T+n7tGGeOSL1ZpcZa6eDUhyUD01C2+EQXAeywgW2BgdB04t89/1O/w1cDnyilFU="
+    channelSecret: ChannelAccessToken,
+    channelAccessToken: ChannelSecret
 });
 
 bot.on('message', function(event) {
