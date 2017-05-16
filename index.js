2742fb65acd490ea30cb19e2f4d1a6c34cafbf79
Ray <marsray@yxunistar.com>
2017-05-16 14:55:51 +0800

    add reply

--- index.js
+++ index.js
@@ -20,20 +20,21 @@ bot.on('message', function(event) {
     console.log("BOT GET A MESSAGE:", event); //把收到訊息的 event 印出來看看
     if (event.message.type = 'text') {
         var msg = "";
-        if ( "RAY" === event.message.text.toUpperCase() ) {
-            msg = "造物主";
-        } else {
-            switch(event.message.type) {
-                case "sticker" :
-                    msg = "貼圖";
-                    if ('1252298' === event.message.packageId) {
-                        msg += " '喵尼愛撒嬌'";
-                    }
-                    break;
-                default:
-                    msg = event.message.type;
-                    break;
-            }
+        switch(event.message.type) {
+            case "sticker" :
+                msg = "貼圖";
+                if ('1252298' === event.message.packageId) {
+                    msg += " '喵尼愛撒嬌'";
+                }
+                break;
+            case "text":
+                if ("RAY" === event.message.text.toUpperCase()) {
+                    msg = "造物主";
+                }
+                break;
+            default:
+                msg = event.message.type;
+                break;
         }
 
         event.reply(msg).then(function(data) {
